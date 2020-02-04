7ec962805b4d197eeed0d614938f6d07d59560d2
Prakash Choudhary <om.pcprakash@gmail.com>
2020-02-04 23:34:12 +0530

    issue 2 solved with compro of animation

--- scripts/box.js
+++ scripts/box.js
@@ -1,22 +1,22 @@
-const cardsArray = ['X', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd'];
+const cardsArray = ["X", "A", "a", "B", "b", "C", "c", "D", "d"];
 
 function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-    return array;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
 }
 
 shuffle(cardsArray);
@@ -29,7 +29,6 @@ h1.style.fontSize = "3rem";
 h1.style.color = "maroon";
 h1.appendChild(node);
 
-
 var p = document.createElement("p");
 var node = document.createTextNode("MOVES: ");
 p.setAttribute("id", "info");
@@ -41,44 +40,37 @@ var node = document.createTextNode("0");
 span.setAttribute("id", "score");
 span.appendChild(node);
 
-
 var element = document.getElementById("heading");
 element.appendChild(h1);
 element.appendChild(p);
 var element = document.getElementById("info");
 element.appendChild(span);
 
-
-
 cardsArray.forEach(card => {
-    var div = document.createElement("div");
-    var node = document.createTextNode("");
-    div.appendChild(node);
-    div.setAttribute("class", "cards");
-    div.setAttribute("id", "card" + card);
-    div.setAttribute("data-name", card);
+  var div = document.createElement("div");
+  var node = document.createTextNode("");
+  div.appendChild(node);
+  div.setAttribute("class", "cards");
+  div.setAttribute("id", "card" + card);
+  div.setAttribute("data-name", card);
 
+  var span1 = document.createElement("span");
+  var node = document.createTextNode("" + card.toUpperCase());
+  span1.setAttribute("class", "front");
+  span1.appendChild(node);
 
-    var span1 = document.createElement("span");
-    var node = document.createTextNode("" + card.toUpperCase());
-    span1.setAttribute("class", "front");
-    span1.appendChild(node);
+  var span2 = document.createElement("span");
+  var node = document.createTextNode("0");
+  span2.setAttribute("class", "back");
+  span2.appendChild(node);
 
-
-    var span2 = document.createElement("span");
-    var node = document.createTextNode("0");
-    span2.setAttribute("class", "back");
-    span2.appendChild(node);
-
-    var element = document.getElementById("memoryGame");
-    element.appendChild(div);
-    var element = document.getElementById("card" + card);
-    element.appendChild(span1);
-    var element = document.getElementById("card" + card);
-    element.appendChild(span2);
+  var element = document.getElementById("memoryGame");
+  element.appendChild(div);
+  var element = document.getElementById("card" + card);
+  element.appendChild(span2);
+  var element = document.getElementById("card" + card);
+  element.appendChild(span1);
 });
-
-
 
 var button = document.createElement("button");
 var node = document.createTextNode("Restart Game");
@@ -89,16 +81,10 @@ button.appendChild(node);
 var element = document.getElementById("heading");
 element.appendChild(button);
 
-
-
 // <div class="cards" data-name="A">
 // <span class="front">A</span>
 // <span class="back">0</span>
 // </div>Z
-
-
-
-
 
 // var tree = document.createDocumentFragment();
 
@@ -108,7 +94,6 @@ element.appendChild(button);
 // p.appendChild(document.createTextNode("Moves"));
 // var span = document.createElement("span");
 // span.setAttribute("id", "score");
-
 
 // tree.appendChild(p);
 // tree.appendChild(span);
